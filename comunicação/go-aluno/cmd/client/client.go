15267cafeb8978f37a5fd797782e54a630eb2a94
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2021-03-25 12:07:34 -0300

    feat(comunicaçao/go-aluno): criando client streaming (completo)

--- comunicação/go-aluno/cmd/client/client.go
+++ comunicação/go-aluno/cmd/client/client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
+	"time"
 
 	"github.com/codeedu/fc2-grpc/pb"
 	"google.golang.org/grpc"
@@ -18,7 +20,9 @@ func main() {
 	defer connection.Close()
 
 	client := pb.NewUserServiceClient(connection)
-	AddUser(client)
+	// AddUser(client)
+	// AddUserVerbose(client)
+	AddUsers(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -31,6 +35,81 @@ func AddUser(client pb.UserServiceClient) {
 	res, err := client.AddUser(context.Background(), req)
 	if err != nil {
 		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	fmt.Println(res)
+}
+
+func AddUserVerbose(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "João",
+		Email: "j@j.com",
+	}
+
+	responseStream, err := client.AddUserVerbose(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	for {
+		stream, err := responseStream.Recv()
+
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Could not receive the message: %v", err)
+		}
+
+		fmt.Println("Status:", stream.Status, " - ", stream.GetUser())
+	}
+}
+
+func AddUsers(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "l1",
+			Name:  "Lucas",
+			Email: "lucas@lucas.com",
+		},
+		&pb.User{
+			Id:    "l2",
+			Name:  "Lucas 2",
+			Email: "lucas2@lucas.com",
+		},
+		&pb.User{
+			Id:    "l3",
+			Name:  "Lucas 3",
+			Email: "lucas3@lucas.com",
+		},
+		&pb.User{
+			Id:    "l4",
+			Name:  "Lucas 4",
+			Email: "lucas4@lucas.com",
+		},
+		&pb.User{
+			Id:    "l5",
+			Name:  "Lucas 5",
+			Email: "lucas5@lucas.com",
+		},
+	}
+
+	stream, err := client.AddUsers(context.Background())
+
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	for _, req := range reqs {
+		stream.Send(req)
+		time.Sleep(time.Second * 3)
+	}
+
+	res, err := stream.CloseAndRecv()
+
+	if err != nil {
+		log.Fatalf("Error receiving response: %v", err)
 	}
 
 	fmt.Println(res)
