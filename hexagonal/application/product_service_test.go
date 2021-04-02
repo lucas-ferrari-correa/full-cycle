a281a166ad4e083089ab96ca5566376bbdcaa7fe
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2021-04-02 16:17:49 -0300

    feat(hexagonal): trabalhando com gomock e mockgen (completo)

--- hexagonal/application/product_service_test.go
+++ hexagonal/application/product_service_test.go
@@ -1,1 +1,27 @@
 package application_test
+
+import (
+	"testing"
+
+	"github.com/codeedu/go-hexagonal/application"
+	mock_application "github.com/codeedu/go-hexagonal/application/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProductService_Get(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockProductInteface(ctrl)
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
