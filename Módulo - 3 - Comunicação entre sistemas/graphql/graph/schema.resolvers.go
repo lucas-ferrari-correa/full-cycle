74af2da750c21112ebeda574c78471f500371c1e
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2022-11-02 21:18:37 -0300

    graphql2

--- Módulo - 3 - Comunicação entre sistemas/graphql/graph/schema.resolvers.go
+++ Módulo - 3 - Comunicação entre sistemas/graphql/graph/schema.resolvers.go
@@ -20,12 +20,26 @@ func (r *categoryResolver) Courses(ctx context.Context, obj *model.Category) ([]
 	var coursesModel []*model.Course
 	for _, course := range courses {
 		coursesModel = append(coursesModel, &model.Course{
-			ID: course.ID,
-			Name: course.Name,
+			ID:          course.ID,
+			Name:        course.Name,
 			Description: &course.Description,
 		})
 	}
 	return coursesModel, nil
+}
+
+// Category is the resolver for the category field.
+func (r *courseResolver) Category(ctx context.Context, obj *model.Course) (*model.Category, error) {
+	category, err := r.CategoryDB.FindByCourseID(obj.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &model.Category{
+		ID:          category.ID,
+		Name:        category.Name,
+		Description: &category.Description,
+	}, nil
 }
 
 // CreateCategory is the resolver for the createCategory field.
@@ -99,6 +113,9 @@ func (r *queryResolver) Courses(ctx context.Context) ([]*model.Course, error) {
 // Category returns generated.CategoryResolver implementation.
 func (r *Resolver) Category() generated.CategoryResolver { return &categoryResolver{r} }
 
+// Course returns generated.CourseResolver implementation.
+func (r *Resolver) Course() generated.CourseResolver { return &courseResolver{r} }
+
 // Mutation returns generated.MutationResolver implementation.
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
 
@@ -106,5 +123,6 @@ func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResol
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 type categoryResolver struct{ *Resolver }
+type courseResolver struct{ *Resolver }
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
