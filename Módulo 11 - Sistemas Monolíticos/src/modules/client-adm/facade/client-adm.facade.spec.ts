aed7c10b21c2a6ea4b06d58f133249257a4184bc
Lucas Ferrari <lucas.ferrari@growth.sale>
2023-06-07 08:15:07 -0300

    Módulo ClientAdm finished

---
+++ Módulo 11 - Sistemas Monolíticos/src/modules/client-adm/facade/client-adm.facade.spec.ts
@@ -0,0 +1,79 @@
+import { Sequelize } from "sequelize-typescript";
+import { ClientModel } from "../repository/client.model";
+import ClientRepository from "../repository/client.repository";
+import AddClientUseCase from "../usecase/add-client/add-client.usecase";
+import ClientAdmFacade from "./client-adm.facade";
+import FindClientUseCase from "../usecase/find-client/find-client.usecase";
+
+describe("ClientAdmFacade test", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true }
+    });
+
+    await sequelize.addModels([ClientModel]);
+    await sequelize.sync();
+  })
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should create a client", async () => {
+    const repository = new ClientRepository();
+    const addUseCase = new AddClientUseCase(repository);
+    const facade = new ClientAdmFacade({
+      addUsecase: addUseCase,
+      findUsecase: undefined,
+    });
+
+    const input = {
+      id: "1",
+      name: "Client 1",
+      email: "x@x.com",
+      address: "Address 1"
+    }
+
+    await facade.add(input);
+
+    const client = (await ClientModel.findOne({ where: { id: "1" } })).dataValues;
+
+    expect(client).toBeDefined();
+    expect(client.name).toBe(input.name);
+    expect(client.email).toBe(input.email);
+    expect(client.address).toBe(input.address);
+  });
+
+  it("should find a client", async () => {
+    const repository = new ClientRepository();
+    const findUsecase = new FindClientUseCase(repository);
+    const facade = new ClientAdmFacade({
+      addUsecase: undefined,
+      findUsecase: findUsecase,
+    });
+
+    const input = {
+      id: "1",
+      name: "Client 1",
+      email: "x@x.com",
+      address: "Address 1",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }
+
+    await ClientModel.create(input);
+
+    const client = await facade.find({ id: "1" });
+
+    expect(client).toBeDefined();
+    expect(client.id).toBeDefined();
+    expect(client.name).toBe(input.name);
+    expect(client.email).toBe(input.email);
+    expect(client.address).toBe(input.address);
+  })
+})
