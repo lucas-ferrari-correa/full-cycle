573197350c8f347854b0b0c3195b99babb0d27cb
Lucas Ferrari <lucas.ferrari@growth.sale>
2023-07-05 11:24:48 -0300

    Microsserviço WalletCore almost finished

---
+++ Módulo 12 - Arquitetura baseada em microsserviços/walletcore/internal/gateway/transaction.go
@@ -0,0 +1,9 @@
+package gateway
+
+import (
+	"github.com.br/devfullcycle/fc-ms-wallet/internal/entity"
+)
+
+type TransactionGateway interface {
+	Create(transaction *entity.Transaction) error
+}
---
+++ Módulo 12 - Arquitetura baseada em microsserviços/walletcore/internal/usecase/create_transaction/create_transaction_test.go
@@ -0,0 +1,63 @@
+package createtransaction
+
+import (
+	"github.com.br/devfullcycle/fc-ms-wallet/internal/entity"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+type TransactionGatewayMock struct {
+	mock.Mock
+}
+
+func (m *TransactionGatewayMock) Create(transaction *entity.Transaction) error {
+	args := m.Called(transaction)
+	return args.Error(0)
+}
+
+type AccountGatewayMock struct {
+	mock.Mock
+}
+
+func (m *AccountGatewayMock) Save(account *entity.Account) error {
+	args := m.Called(account)
+	return args.Error(0)
+}
+
+func (m *AccountGatewayMock) FindByID(id string) (*entity.Account, error) {
+	args := m.Called(id)
+	return args.Get(0).(*entity.Account), args.Error(1)
+}
+
+func TestCreateTransactionUseCase_Execute(t *testing.T) {
+	client1, _ := entity.NewClient("client1", "j@j")
+	account1 := entity.NewAccount(client1)
+	account1.Credit(1000)
+
+	client2, _ := entity.NewClient("client2", "j@j2")
+	account2 := entity.NewAccount(client2)
+	account2.Credit(1000)
+
+	accountMock := &AccountGatewayMock{}
+	accountMock.On("FindByID", account1.ID).Return(account1, nil)
+	accountMock.On("FindByID", account2.ID).Return(account2, nil)
+
+	mockTransaction := &TransactionGatewayMock{}
+	mockTransaction.On("Create", mock.Anything).Return(nil)
+
+	inputDto := CreateTransactionInputDTO{
+		AccountIDFrom: account1.ID,
+		AccountIDTo:   account2.ID,
+		Amount:        100,
+	}
+
+	uc := NewCreateTransactionUseCase(mockTransaction, accountMock)
+	output, err := uc.Execute(inputDto)
+	assert.Nil(t, err)
+	assert.NotNil(t, output)
+	accountMock.AssertExpectations(t)
+	mockTransaction.AssertExpectations(t)
+	accountMock.AssertNumberOfCalls(t, "FindByID", 2)
+	mockTransaction.AssertNumberOfCalls(t, "Create", 1)
+}
