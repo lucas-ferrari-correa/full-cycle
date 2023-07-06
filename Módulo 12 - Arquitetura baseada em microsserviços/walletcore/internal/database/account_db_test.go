3876e1a07f6ad64948060feea8b7e7b4020d7807
Lucas Ferrari <lucas.ferrari@growth.sale>
2023-07-06 11:31:10 -0300

    Arquitetura baseada em microsserviços finished

---
+++ Módulo 12 - Arquitetura baseada em microsserviços/walletcore/internal/database/account_db_test.go
@@ -0,0 +1,62 @@
+package database
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com.br/devfullcycle/fc-ms-wallet/internal/entity"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/suite"
+)
+
+type AccountDBTestSuite struct {
+	suite.Suite
+	db *sql.DB
+	accountDB *AccountDB
+	client *entity.Client
+}
+
+func (s *AccountDBTestSuite) SetupSuite() {
+	db, err := sql.Open("sqlite3", ":memory:")
+	s.Nil(err)
+	s.db = db
+
+	db.Exec("Create table clients (id varchar(255), name varchar(255), email varchar(255), created_at date)")
+	db.Exec("Create table accounts (id varchar(255), client_id varchar(255), balance int, created_at date)")
+	s.accountDB = NewAccountDB(db)
+	s.client, _ = entity.NewClient("John", "j@j.com")
+}
+
+func (s *AccountDBTestSuite) TearDownSuite() {
+	defer s.db.Close()
+	s.db.Exec("DROP TABLE clients")
+	s.db.Exec("DROP TABLE accounts")
+}
+
+func TestAccountDBTestSuit(t *testing.T) {
+	suite.Run(t, new(AccountDBTestSuite))
+}
+
+func (s *AccountDBTestSuite) TestSave() {
+	account := entity.NewAccount(s.client)
+	err := s.accountDB.Save(account)
+	s.Nil(err)
+}
+
+func (s *AcountDBTestSuite) TestFindByID() {
+	s.db.Exec("Insert into clients (id, name, email, created_at) values (?, ?, ?, ?)",
+		s.client.ID, s.client.Name, s.client.Email, s.client.CreatedAt
+	)
+
+	account := entity.NewAccount(s.client)
+	err := s.acountDB.Save(account)
+	s.Nil(err)
+	accountDB, err := s.accountDB.FindByID(account.ID)
+	s.Nil(err)
+	s.Equal(account.ClientID, accountDB.ClientID)
+	s.Equal(account.Name, accountDB.Name)
+	s.Equal(account.Balance, accountDB.Balance)
+	s.Equal(account.Client.ID, accountDB.Client.ID)
+	s.Equal(account.Client.Name, accountDB.Client.Name)
+	s.Equal(account.Client.Email, accountDB.Client.Email)
+}
